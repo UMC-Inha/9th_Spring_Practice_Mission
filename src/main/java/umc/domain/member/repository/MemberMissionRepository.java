c829f96c7517100078ce664787b18f595f532ee9
zeoueon <zeoueon129@gmail.com>
2025-10-29 16:33:46 +0900

    feat: 지역별 도전 가능한 미션 목록 쿼리 작성
    지역별 도전 가능한 미션 목록을 추출하는 쿼리를 정의한다.
    미션과 가게정보, 마감 기한을 저장하는 DTO를 정의한다.

--- src/main/java/umc/domain/member/repository/MemberMissionRepository.java
+++ src/main/java/umc/domain/member/repository/MemberMissionRepository.java
@@ -3,6 +3,7 @@ package umc.domain.member.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import umc.domain.member.dto.MissionChallengeListDto;
 import umc.domain.member.dto.MissionListDto;
 import umc.domain.member.entity.mapping.MemberMission;
 import umc.domain.member.enums.Status;
@@ -37,4 +38,25 @@ public interface MemberMissionRepository {
     long getMissionCountByMemberIdAndRegion(@Param("member_id")  Long member_id,
                                            @Param("region_name") String region_name,
                                             @Param("status") Status status);
+
+    @Query("""
+        select new umc.domain.member.dto.MissionChallengeListDto(s.name, s.type
+                , ms.description
+                , ms.points
+                , function('datediff',ms.endDate, current_date())
+                , ms.id)
+        from Mission ms
+            join ms.store s
+            join s.region r
+        where function('datediff',ms.endDate, current_date()) >= 0
+            and r.name = :region_name
+            and not exists (
+                select 1
+                from MemberMission mm
+                where mm.mission.id = ms.id
+                    and mm.member.id = :member_id
+            )
+""")
+    List<MissionChallengeListDto> findAvailableMissionsByMemberIdAndRegion(@Param("region_name") String region_name,
+                                                                           @Param("member_id") Long member_id);
 }
