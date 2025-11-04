363c0f10844ee14b4fb5bb0cc8ecc1c266b489f3
zeoueon <zeoueon129@gmail.com>
2025-11-04 23:45:15 +0900

    feat(ReviewQueryService): ReviewQuery의 서비스 계층 생성 및 구현
    BooleanBuilder 를 이용해 동적으로 조건을 적용하여 쿼리문을 호출하는 함수를 정의함.

---
+++ src/main/java/umc/domain/review/service/ReviewQueryService.java
@@ -0,0 +1,32 @@
+package umc.domain.review.service;
+
+import com.querydsl.core.BooleanBuilder;
+import java.math.BigDecimal;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import umc.domain.review.dto.ReviewDto;
+import umc.domain.review.entity.QReview;
+import umc.domain.review.repository.ReviewRepository;
+
+@Service
+public class ReviewQueryService {
+
+    private ReviewRepository reviewRepository;
+
+    public List<ReviewDto> searchReviewByMember(Long id, String storeName, BigDecimal starRating) {
+        QReview review = QReview.review;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (id != null) {
+            builder.and(review.member.id.eq(id));
+        }
+        if (storeName != null) {
+            builder.and(review.store.name.eq(storeName));
+        }
+        if (starRating != null) {
+            builder.and(review.starRating.eq(starRating));
+        }
+
+        return reviewRepository.searchReviewByMemberId(builder);
+    }
+}
