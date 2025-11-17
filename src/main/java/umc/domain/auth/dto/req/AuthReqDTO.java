ea38e410708de268b549651b9ae4e1b434a9325f
minjaehan <tommy3942@naver.com>
2025-11-17 23:02:31 +0900

    feat: AuthDTO 구성

---
+++ src/main/java/umc/domain/auth/dto/req/AuthReqDTO.java
@@ -0,0 +1,19 @@
+package umc.domain.auth.dto.req;
+
+import umc.domain.member.enums.Address;
+import umc.domain.member.enums.Gender;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class AuthReqDTO {
+
+    public record SignUpDTO(
+       String name,
+       Gender gender,
+       LocalDate  birth,
+       Address address,
+       List<Long> preferCategory
+
+    ){}
+}
