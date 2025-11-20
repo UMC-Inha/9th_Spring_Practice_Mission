267d666c84ea22c9d38f42a0f686a1582ddbabb9
haewonee <godnjs5870@naver.com>
2025-11-21 00:13:30 +0900

    feat: 페이지 커스텀 어노테이션 구현

---
+++ src/main/java/umc/global/validator/CheckPageValidator.java
@@ -0,0 +1,26 @@
+package umc.global.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import umc.global.annotation.CheckPage;
+import umc.global.apiPayload.code.GeneralErrorCode;
+
+public class CheckPageValidator implements ConstraintValidator<CheckPage,Integer> {
+
+    @Override
+    public void initialize(CheckPage constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+        if(value == null || value < 1){
+            context.disableDefaultConstraintViolation();//기본 메시지 끄기
+            //커스텀 에러 메세지 설정
+            context.buildConstraintViolationWithTemplate(GeneralErrorCode.BAD_REQUEST.getMessage())
+                    .addConstraintViolation();
+            return false;
+        }
+        return true;
+    }
+}
