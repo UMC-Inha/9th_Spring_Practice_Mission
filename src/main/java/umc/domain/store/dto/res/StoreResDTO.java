c5105bc880f5efa924e8653583efeefa6a43f3f0
minjaehan <tommy3942@naver.com>
2025-11-21 15:48:16 +0900

    fix: storeInfo record로 변경

--- src/main/java/umc/domain/store/dto/res/StoreResDTO.java
+++ src/main/java/umc/domain/store/dto/res/StoreResDTO.java
@@ -5,13 +5,12 @@ import lombok.Getter;
 
 public class StoreResDTO {
 
-    @Getter
     @Builder
-    public static class StoreInfo{
-        private Long storeId;
-        private String name;
-        private Long managerNumber;
-        private String detailAddress;
-        private String locationName;
-    }
+    public record StoreInfo(
+        Long storeId,
+        String name,
+        Long managerNumber,
+        String detailAddress,
+        String locationName
+    ){}
 }
