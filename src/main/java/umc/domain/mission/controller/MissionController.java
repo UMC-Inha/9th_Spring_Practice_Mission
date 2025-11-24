b88329dab974106ca743f8a80eeeb5710f62caf1
minjaehan <tommy3942@naver.com>
2025-11-24 21:29:59 +0900

    refactor: mission controller 엔드포인트 재정립

--- src/main/java/umc/domain/mission/controller/MissionController.java
+++ src/main/java/umc/domain/mission/controller/MissionController.java
@@ -1,24 +1,71 @@
-package umc.domain.mission.controller;
+﻿package umc.domain.mission.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import umc.domain.mission.dto.req.MissionReqDTO;
 import umc.domain.mission.dto.res.MissionResDTO;
+import umc.domain.mission.service.MemberMissionCommandService;
+import umc.domain.mission.service.MemberMissionQueryService;
+import umc.domain.mission.service.MissionCreateService;
 import umc.domain.mission.service.MissionQueryService;
 import umc.global.apiPayload.ApiResponse;
 import umc.global.apiPayload.code.GeneralSuccessCode;
 
 @RestController
 @RequiredArgsConstructor
-
+@RequestMapping("/api/missions")
 public class MissionController {
 
-    private final MissionQueryService  missionQueryService;
+    private final MissionQueryService missionQueryService;
+    private final MissionCreateService missionCreateService;
+    private final MemberMissionQueryService memberMissionQueryService;
+    private final MemberMissionCommandService memberMissionCommandService;
 
-    @GetMapping("/api/missions/location")
-    public ApiResponse<   MissionResDTO.MissionListResult> findByLocationName(String locationName, Pageable pageable) {
+    @GetMapping
+    public ApiResponse<MissionResDTO.MissionListResult> findByLocation(
+            @RequestParam String locationName,
+            @PageableDefault(size = 10) Pageable pageable
+    ) {
         MissionResDTO.MissionListResult result = missionQueryService.findByLocationName(locationName, pageable);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, result);
+    }
+
+    @PostMapping("/stores/{storeId}")
+    public ApiResponse<MissionResDTO.MissionDetailDTO> createMission(
+            @PathVariable Long storeId,
+            @RequestBody MissionReqDTO.MissionCreateReq req
+    ) {
+        MissionResDTO.MissionDetailDTO result = missionCreateService.createMission(storeId, req);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, result);
+    }
+
+    @PostMapping("/members/{memberId}")
+    public ApiResponse<MissionResDTO.GetChallengeMissionResDTO> challengeMission(
+            @PathVariable Long memberId,
+            @RequestBody MissionReqDTO.GetChallengeMissionReqDTO request
+    ) {
+        MissionResDTO.GetChallengeMissionResDTO result =
+                memberMissionCommandService.getChallengeMission(memberId, request.getMissionId());
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, result);
+    }
+
+    @GetMapping("/members/{memberId}/ongoing")
+    public ApiResponse<MissionResDTO.OnGoingMissionListResult> getOnGoingMissions(
+            @PathVariable Long memberId,
+            @PageableDefault(page = 0, size = 10) Pageable pageable
+    ) {
+        MissionResDTO.OnGoingMissionListResult result = memberMissionQueryService.getOnGoingMissions(memberId, pageable);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, result);
+    }
+
+    @GetMapping("/members/{memberId}/completed")
+    public ApiResponse<MissionResDTO.CompletedMissionListResult> getCompletedMissions(
+            @PathVariable Long memberId,
+            @PageableDefault(page = 0, size = 10) Pageable pageable
+    ) {
+        MissionResDTO.CompletedMissionListResult result = memberMissionQueryService.getCompletedMissions(memberId, pageable);
         return ApiResponse.onSuccess(GeneralSuccessCode.OK, result);
     }
 }
